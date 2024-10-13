5a95c44206b753b0ebfffccd19170e7fe4f8a999
khekrn <kishorenayark@gmail.com>
2024-10-13 18:50:37 +0530

    Completed the day2 part 1 challenge in golang

---
+++ go-advc/2015/day2/day2.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// --- Day 2: I Was Told There Would Be No Math ---
+// The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions
+// (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
+
+// Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a
+// little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present:
+// the area of the smallest side.
+
+// For example:
+
+// A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+// A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+// All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
+
+//go:embed input.txt
+var input string
+
+func part1() int {
+	var result int
+	for _, line := range strings.Split(input, "\n") {
+		if line == "" {
+			break
+		}
+		dims := strings.Split(line, "x")
+		l, err := strconv.Atoi(dims[0])
+		w, err := strconv.Atoi(dims[1])
+		h, err := strconv.Atoi(dims[2])
+
+		if err != nil {
+			fmt.Println("Error converting dimensions to integers")
+			break
+		}
+
+		area := 2*l*w + 2*w*h + 2*h*l
+		minArea := min(l*w, w*h, h*l)
+		result += area + minArea
+	}
+	return result
+}
+
+func part2() int {
+	return 0
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running Part = ", part)
+	result := 0
+	if part == 1 {
+		result = part1()
+	} else {
+		result = part2()
+	}
+	fmt.Println("Result = ", result)
+}
